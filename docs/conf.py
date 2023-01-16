fc6afdc7d31ddb6abd08e7ccdcd4c602d022d82f
Danguilhen <thomas.danguilhen@estaca.eu>
2023-01-16 21:51:29 +0100

    add searchtools

--- docs/conf.py
+++ docs/conf.py
@@ -39,7 +39,7 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "sphinx_rtd_theme"  # "furo"
-html_static_path = []
+html_static_path = ["_static"]
 
 import pathlib
 
